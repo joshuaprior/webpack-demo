bd02bae7bf490178b153c78d89c6c1cc5483a4b8
Josh Prior <joshua_prior@intuit.com>
2016-02-08 12:54:20 -0800

    Switch to use babel-cli without webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var child_process = require('child_process');
+
 module.exports = function(grunt) {
     grunt.initConfig({
         connect: {
@@ -13,5 +15,21 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('default', ['connect:server']);
+
+    grunt.registerTask('clean', function() {
+        child_process.execSync('rm -rf public/dist/');
+    });
+
+    grunt.registerTask('build', function() {
+        child_process.execSync('mkdir public/dist');
+        child_process.execSync('./node_modules/.bin/babel src -o public/dist/bundle.js --source-maps');
+    });
+
+    grunt.registerTask('copyjs', function() {
+        child_process.execSync('mkdir public/dist');
+        child_process.execSync('cp src/demo.js public/dist/bundle.js');
+    });
+
+    grunt.registerTask('default', ['clean', 'build', 'connect:server']);
+    grunt.registerTask('no-babel', ['clean', 'copyjs', 'connect:server']);
 };
---
+++ public/dist/bundle.js
@@ -0,0 +1,19 @@
+debugger;
+
+[1,2,3,4,5]
+    .map(n => n * 100)
+    .forEach(n => console.log(n));
+
+var foo = {
+    func: () => {}
+};
+
+var bar = foo.func({
+    baz: () => {
+        var qux = Math.round(0);
+    }
+});
+
+[1,2,3,4,5]
+    .map(n => n * 100)
+    .forEach(n => console.log(n));
